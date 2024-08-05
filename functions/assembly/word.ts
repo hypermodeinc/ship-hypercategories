773008ceb212c81825d7dbeed8545de5597c0373
Matt Johnson-Pint <mjp@hypermode.com>
2024-08-05 10:26:48 -0700

    update for eslint changes

--- functions/assembly/word.ts
+++ functions/assembly/word.ts
@@ -1,19 +1,20 @@
-import {  http} from "@hypermode/functions-as";
-import { JSON } from "json-as";
+import { http } from "@hypermode/functions-as";
 
-
-export function isEnglishWord(input: string) : boolean {
+export function isEnglishWord(input: string): boolean {
   // get first word
-  const word = input.split(" ")[0].trim()
-  const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
-  
-  const request = new http.Request(url,{
-    method: "GET"
+  const word = input.split(" ")[0].trim();
+  const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+
+  const request = new http.Request(url, {
+    method: "GET",
   } as http.RequestOptions);
 
   const response = http.fetch(request);
-  
-  const isValid = ! response.text().toLowerCase().includes("no definitions found")
- 
-  return  isValid
+
+  const isValid = !response
+    .text()
+    .toLowerCase()
+    .includes("no definitions found");
+
+  return isValid;
 }
